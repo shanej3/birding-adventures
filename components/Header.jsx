1fc999c618f03ab0fc756480992ce50385ea82c1
Shane <shanejen56@gmail.com>
2025-05-23 13:51:59 -0700

    Header styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,11 @@
+import './Header.css'
+
 function Header() {
     return (
-        <header>
-            <h1>The Shane & Loza Birding Adventures</h1>
+        <header className="header">
+            <h2 className = "header-left">Spring 2025</h2>
+            <h1 className = "header-center">The Shane & Loza Birding Adventures</h1>
+            <h2 className = "header-right">Click on an image for fullscreen!</h2>
         </header>
     )
 }
